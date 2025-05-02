ca20dc2077222af2c4f9f8cf6ef981b1c8e5dbe7
Redstone key <172996447+Redstonekey@users.noreply.github.com>
2025-05-02 16:38:01 +0200

    Update scrap.js

--- scrap.js
+++ scrap.js
@@ -1,13 +1,14 @@
 export class scrap {
     url = "https://start.schulportal.hessen.de";
 
-    name() {
-        fetch(this.url + '/index.php')
-        .then(response => response.text())
-        .then(data => {
+    async name() {
+        try {
+            const response = await fetch(this.url + '/index.php');
+            const data = await response.text();
             const parser = new DOMParser();
             const doc = parser.parseFromString(data, 'text/html');
             const element = doc.querySelector('.fa-child')?.parentElement?.textContent?.trim();
+
             if (element) {
                 const nameMatch = element.match(/([^,]+),\s*([^()]+)\s*\(([^)]+)\)/);
                 if (nameMatch) {
@@ -18,71 +19,73 @@ export class scrap {
                     const className = nameMatch[3].trim();
                     const initials = `${nameMatch[2].split(' ').map(name => name[0]).join('')}${lastName[0]}`;
 
-                    console.log('First Name:', firstName);
-                    console.log('Class:', className);
-                    console.log('Last Name:', lastName);
-                    console.log('Full Name:', fullName);
-                    console.log('Full Name Sph:', fullNameSph);
-                    console.log('Initials:', initials);
-                    document.cookie = `firstName=${firstName}; path=/;`;
-                    document.cookie = `lastName=${lastName}; path=/;`;
-                    document.cookie = `fullName=${fullName}; path=/;`;
-                    document.cookie = `fullNameSph=${fullNameSph}; path=/;`;
-                    document.cookie = `className=${className}; path=/;`;
-                    document.cookie = `initials=${initials}; path=/;`;
+                    return {
+                        firstName: firstName,
+                        lastName: lastName,
+                        fullName: fullName,
+                        fullNameSph: fullNameSph,
+                        className: className,
+                        initials: initials
+                    };
                 } else {
                     console.error('Name format did not match');
+                    return null;
                 }
-            }
-            if (element) {
-                document.open();
-                document.write(`<div>${element}</div>`);
-                document.close();
             } else {
                 console.error('Name could not be fetched');
+                return null;
             }
-        })
-        .catch(error => console.error('Error fetching the page:', error));
+        } catch (error) {
+            console.error('Error fetching the page:', error);
+            return null;
+        }
     }
-    homework() {
-        fetch(this.url + '/meinunterricht.php')
-        .then(response => response.text())
-        .then(data => {
+
+    async homework() {
+        try {
+            const response = await fetch(this.url + '/meinunterricht.php');
+            const data = await response.text();
             const parser = new DOMParser();
             const doc = parser.parseFromString(data, 'text/html');
             const elements = doc.querySelectorAll('.printable[data-entry]');
+            const homeworkResults = [];
+
             elements.forEach(element => {
-                console.log(element);
                 const doneSpan = element.querySelector('.done.label.label-default');
                 if (doneSpan && doneSpan.textContent.trim() === 'erledigt' && !element.textContent.includes('als "erledigt" markieren')) {
-                console.log('Filtered element:', element);
-                const getElementText = (element, xpath) => {
-                    const result = document.evaluate(xpath, element, null, XPathResult.STRING_TYPE, null);
-                    return result.stringValue.trim();
-                };
-    
-                const getElementAttribute = (element, xpath, attribute) => {
-                    const result = document.evaluate(xpath, element, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-                    return result.singleNodeValue ? result.singleNodeValue.getAttribute(attribute) : '';
-                };
-    
-                const className = getElementText(element, './td[1]/h3/a/span');
-                const teacherShort = getElementText(element, './td[1]/span/div/button/text()');
-                const teacherName = getElementText(element, './td[1]/span/div/ul/li[1]/a/text()');
-                const thema = getElementText(element, './td[2]/b');
-                const homework = getElementText(element, './td[2]/div/div');
-                const linkToMore = getElementAttribute(element, './td[3]/div/a', 'href');
-    
-                console.log('Class:', className);
-                console.log('Teacher Short:', teacherShort);
-                console.log('Teacher Name:', teacherName);
-                console.log('Thema:', thema);
-                console.log('Homework:', homework);
-                console.log('Link to More:', linkToMore);
+
+                    const getElementText = (element, xpath) => {
+                        const result = document.evaluate(xpath, element, null, XPathResult.STRING_TYPE, null);
+                        return result.stringValue.trim();
+                    };
+
+                    const getElementAttribute = (element, xpath, attribute) => {
+                        const result = document.evaluate(xpath, element, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+                        return result.singleNodeValue ? result.singleNodeValue.getAttribute(attribute) : '';
+                    };
+
+                    const className = getElementText(element, './td[1]/h3/a/span');
+                    const teacherShort = getElementText(element, './td[1]/span/div/button/text()');
+                    const teacherName = getElementText(element, './td[1]/span/div/ul/li[1]/a/text()');
+                    const thema = getElementText(element, './td[2]/b');
+                    const homework = getElementText(element, './td[2]/div/div');
+                    const linkToMore = getElementAttribute(element, './td[3]/div/a', 'href');
+
+                    homeworkResults.push({
+                        className: className,
+                        teacherShort: teacherShort,
+                        teacherName: teacherName,
+                        thema: thema,
+                        homework: homework,
+                        linkToMore: linkToMore
+                    });
                 }
             });
-        })
-        .catch(error => console.error('Failed to fetch data:', error));
-    }
 
+            return homeworkResults;
+        } catch (error) {
+            console.error('Failed to fetch data:', error);
+            return null;
+        }
+    }
 }
